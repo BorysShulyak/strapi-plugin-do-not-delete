97e9e2d816bb5de65b5a9a069847154f9ea81713
boris.shulyak <borysshulyak@gmail.com>
2023-07-12 12:50:09 +0300

    Implement `notIn`, `hasNot`  comparators.

--- server/utils/is-protected-entity.js
+++ server/utils/is-protected-entity.js
@@ -4,7 +4,9 @@ const COMPARATORS = {
   is: 'is',
   isNot: 'isNot',
   in: 'in',
+  notIn: 'notIn',
   has: 'has',
+  hasNot: 'hasNot',
   matches: 'matches',
 };
 
@@ -12,7 +14,9 @@ const COMPARATOR_ACTION_STRATEGY = {
   [COMPARATORS.is]: (value, entityAttr) => value === entityAttr,
   [COMPARATORS.isNot]: (value, entityAttr) => value !== entityAttr,
   [COMPARATORS.in]: (value, entityAttr) => value.includes(entityAttr),
+  [COMPARATORS.notIn]: (value, entityAttr) => !value.includes(entityAttr),
   [COMPARATORS.has]: (value, entityAttr) => entityAttr.includes(value),
+  [COMPARATORS.hasNot]: (value, entityAttr) => !entityAttr.includes(value),
   [COMPARATORS.matches]: (value) => RegExp(value).test(),
 };
 
